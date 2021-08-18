56bda9b96879543bcf66e5548e4c1e40faf2821b
niarcotyk <jouhanneauanne_laure@yahoo.fr>
2021-08-18 10:19:56 +0200

    modification accueil

--- src/Form/SortieRechercheType.php
+++ src/Form/SortieRechercheType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
